fdfad8d1a5685fee162875c3bec900bb2a68b62a
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-08-04 13:45:18 +0300

    Create archetype Kotlin project

---
+++ src/main/kotlin/vorpality/Hello.kt
@@ -0,0 +1,6 @@
+package vorpality
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/vorpality/HelloTest.kt
@@ -0,0 +1,8 @@
+package vorpality
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
